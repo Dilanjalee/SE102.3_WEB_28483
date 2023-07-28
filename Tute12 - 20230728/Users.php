c540718239aa9f3bd85e8797558e4b9153b57746
Dilanjalee S Rajapaksha <136807659+DilanjaleeSRajapaksha@users.noreply.github.com>
2023-07-28 16:43:56 +0530

    Add files via upload

---
+++ Tute12 - 20230728/Users.php
@@ -0,0 +1,43 @@
+<html>
+    <body>
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $db = "sample";
+
+            $conn = mysqli_connect($servername, $username, $password, $db);
+
+            if(!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            $sql = "SELECT id, name, email FROM users";
+            $result = $conn->query($sql);
+        ?>
+
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+
+            <?php
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) { ?>
+                    <tr>
+                        <td> <?php echo $row["id"] ?> </td>
+                        <td> <?php echo $row["name"] ?></td>
+                        <td> <?php echo $row["email"] ?> </td>
+                    </tr>
+                    <?php }
+                    
+                } else {
+                    echo "0 results";
+                }
+                ?>
+        </table>
+    </body>
+</html>
+
